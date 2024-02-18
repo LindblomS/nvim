e84d06d0f436504be8d5eb5a8307fc7ff26d622f
sappe <samuel.lindblom@hotmail.com>
2024-02-18 22:51:04 +0100

    Configured telescope layout

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,5 @@
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", '<leader>ff', builtin.find_files, {})
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set('n', 'gr', builtin.lsp_references, {})
 vim.keymap.set('n', 'gs', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})
@@ -9,3 +8,13 @@ vim.keymap.set('n', 'gi', builtin.lsp_implementations, {})
 vim.keymap.set('n', '<leader>d', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
+
+require('telescope').setup {
+    defaults = {
+        layout_strategy = 'horizontal',
+        layout_config = {
+            width = 0.9
+        }
+    },
+
+}

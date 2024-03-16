36c1b5ab481a9c7bd2c7d7b0e2cc5cf5d6dbb6e6
sappe <samuel.lindblom@hotmail.com>
2024-03-16 15:10:40 +0100

    refactored telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,9 +1,4 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
     'neovim/nvim-lspconfig',

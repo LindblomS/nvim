13501bc27abf0a6f8805e1adcc6b106f0fa6267d
sappe <samuel.lindblom@hotmail.com>
2024-09-22 11:13:59 +0200

    Added html support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,7 +5,7 @@ return {
             local lspconfig = require('lspconfig')
 
             -- servers that doesn't require any custom setup
-            local servers = { 'rust_analyzer', 'tsserver', 'volar' }
+            local servers = { 'rust_analyzer', 'tsserver', 'volar', "html" }
             for _, server in ipairs(servers) do
                 lspconfig[server].setup({})
             end
--- lua/plugins/syntax_highlight.lua
+++ lua/plugins/syntax_highlight.lua
@@ -5,7 +5,7 @@ return {
         config = function()
             local configs = require('nvim-treesitter.configs')
             configs.setup({
-                ensure_installed = { 'lua', 'rust', 'c_sharp', 'vim', 'toml', 'typescript', 'javascript', 'vue' },
+                ensure_installed = { 'lua', 'rust', 'c_sharp', 'vim', 'toml', 'typescript', 'javascript', 'vue', "html", "css", "json" },
                 sync_install = false,
                 highlight = { enable = true },
                 indent = { enable = true },

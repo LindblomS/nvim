7c42b88ead211e3bbf88785bb62e23cd02a8c591
Samuel Lindblom <samuel.lindblom@stratsys.se>
2024-10-31 08:55:18 +0100

    Added jsonls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,7 +5,7 @@ return {
             local lspconfig = require('lspconfig')
 
             -- servers that doesn't require any custom setup
-            local servers = { 'rust_analyzer', "html" }
+            local servers = { 'rust_analyzer', "html", "jsonls" }
             for _, server in ipairs(servers) do
                 lspconfig[server].setup({})
             end
